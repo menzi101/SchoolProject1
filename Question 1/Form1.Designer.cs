58a18c8fb7a057bafd4926ec87843fdeb8c2aaf1
menzi101 <menzi.hlope@gmail.com>
2018-01-12 21:39:35 +0200

    Add project files.

---
+++ Question 1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Question_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.clbAnime = new System.Windows.Forms.CheckedListBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblAnimeOfDay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anime List";
+            // 
+            // clbAnime
+            // 
+            this.clbAnime.FormattingEnabled = true;
+            this.clbAnime.Location = new System.Drawing.Point(12, 62);
+            this.clbAnime.Name = "clbAnime";
+            this.clbAnime.Size = new System.Drawing.Size(184, 259);
+            this.clbAnime.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(253, 246);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(244, 86);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(100, 20);
+            this.txtAdd.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(253, 122);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblAnimeOfDay
+            // 
+            this.lblAnimeOfDay.AutoSize = true;
+            this.lblAnimeOfDay.Location = new System.Drawing.Point(12, 339);
+            this.lblAnimeOfDay.Name = "lblAnimeOfDay";
+            this.lblAnimeOfDay.Size = new System.Drawing.Size(94, 13);
+            this.lblAnimeOfDay.TabIndex = 5;
+            this.lblAnimeOfDay.Text = "Anime of the Day: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 361);
+            this.Controls.Add(this.lblAnimeOfDay);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.clbAnime);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Anime List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox clbAnime;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblAnimeOfDay;
+    }
+}
+
---
+++ Question 1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Question_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader SR = new StreamReader("Anime.txt");
+
+            while (SR.EndOfStream == false)
+            {
+                clbAnime.Items.Add(SR.ReadLine());
+            }
+
+            SR.Close();
+
+            Random R = new Random();
+
+            int iNext = R.Next(clbAnime.Items.Count);
+
+            lblAnimeOfDay.Text = "Anime of the Day: " + clbAnime.Items[iNext];
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter SW = new StreamWriter("Favourite.txt");
+
+            for (int i = 0; i < clbAnime.Items.Count; i++)
+            {
+                if (clbAnime.GetItemChecked(i) == true)
+                {
+                    SW.WriteLine(clbAnime.Items[i].ToString());
+                }
+            }
+
+            SW.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtAdd.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter something inside the textbox.", "Missing field", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (clbAnime.Items.Contains(txtAdd.Text) == true)
+                {
+                    MessageBox.Show("This item already exists.", "Already exist", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    clbAnime.Items.Add(txtAdd.Text);
+                    txtAdd.Text = "";
+                    txtAdd.Focus();
+                }
+            }
+        }
+    }
+}
